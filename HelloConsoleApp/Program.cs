038474e0129046a717950cf2661178fa56f122c7
grzegorzkulaga <poczta@grzegorzkulaga.pl>
2019-03-27 19:15:58 +0100

    #1
    Przeniesienie plików

---
+++ HelloConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Program na powitanie.");
+
+            string imie;
+            Console.Write("Wprowadz Imię: ");
+            imie = Console.ReadLine();
+
+            string nazwisko;
+            Console.Write("Wprowadz Nazwisko: ");
+            nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko);
+            */
+            Console.Write("Wprowadz wiek: ");
+            //int wiek = Console.ReadLine();
+            int wiek = Convert.ToInt32(Console.ReadLine());
+
+            if(wiek > 0 && wiek < 67)
+            {
+                Console.WriteLine("Do emerytury zostało Ci {0} lat.", 67 - wiek);
+            }
+            else if(wiek < 0)
+            {
+                Console.WriteLine("Błędne dane!");
+            }
+
+        }
+    }
+}
---
+++ HelloConsoleBatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Program na powitanie.");
+
+            string imie = "";
+            string nazwisko = "";
+            int wiek;
+            /*
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko);
+            
+            Console.Write("Wprowadz wiek: ");
+            //int wiek = Console.ReadLine();
+            */
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Brak danych");
+            }
+            else if (args.Length == 1)
+            {
+                imie = args[0];
+                Console.WriteLine("Witaj" + imie);
+            }
+            else if (args.Length == 2)
+            {
+                imie = args[0];
+                nazwisko = args[1];
+                Console.WriteLine("Witaj" + imie + " " + nazwisko);
+            }
+            else if (args.Length >= 3)
+            {
+                imie = args[0];
+                nazwisko = args[1];
+                wiek = Convert.ToInt32(args[2]);
+
+                if (wiek > 0 && wiek < 67)
+                {
+
+                    Console.WriteLine("Do emerytury zostało Ci {0} lat.", 67 - wiek);
+                }
+                else if (wiek < 0)
+                {
+                    Console.WriteLine("Błędne dane!");
+                }
+                else
+                {
+                    Console.WriteLine("Jesteś emerytem");
+                }
+            }
+        }
+    }
+}
