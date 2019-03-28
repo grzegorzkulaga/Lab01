6d86a71f6bc37fafe2a6b20c6de932b0c97fb24e
grzegorzkulaga <poczta@grzegorzkulaga.pl>
2019-03-28 14:48:23 +0100

    HelloConsoleVBDialog try and catch

--- HelloConsoleVBDialog/Program.cs
+++ HelloConsoleVBDialog/Program.cs
@@ -14,28 +14,48 @@ namespace HelloConsoleVBDialog
             //Interaction.MsgBox("Aplikacja na powitanie!", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Tytuł okienka");
 
             string imie = Interaction.InputBox("Wprowadz imie", "Okienko imię");
-            Console.WriteLine("Podano imię " + imie);
-
-            string nazwisko = Interaction.InputBox("Wprowadz nazwisko", "Okienko nazwisko");
-            Console.WriteLine("Podano nazwisko " + nazwisko);
-
-            Interaction.MsgBox("Witaj " + imie + " " + nazwisko);
-            Interaction.MsgBox(string.Format("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko));
-
-            int wiek = Convert.ToInt32(Interaction.InputBox("Wprowadz wiek ", "Okienko wiek"));
-
-            if (wiek > 0 && wiek < 67)
+            if (imie == "")
             {
-
-                Interaction.MsgBox("Do emerytury zostało Ci " + (67 - wiek) + "lat", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko Emerytura");
-            }
-            else if (wiek < 0)
-            {
-                Interaction.MsgBox("Błędne dane!", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+                Interaction.MsgBox("Koniec", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Koniec");
             }
             else
             {
-                Interaction.MsgBox("Jesteś emerytem", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+                Console.WriteLine("Podano imię " + imie);
+            }
+
+            string nazwisko = Interaction.InputBox("Wprowadz nazwisko", "Okienko nazwisko");
+            if(nazwisko == "")
+            {
+                Interaction.MsgBox("Koniec", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Koniec");
+            }
+            else
+            {
+                Console.WriteLine("Podano nazwisko " + nazwisko);
+                Interaction.MsgBox("Witaj " + imie + " " + nazwisko);
+                Interaction.MsgBox(string.Format("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko));
+            }
+
+            try
+            {
+                int wiek = Convert.ToInt32(Interaction.InputBox("Wprowadz wiek ", "Okienko wiek"));
+
+                if (wiek > 0 && wiek < 67)
+                {
+
+                    Interaction.MsgBox("Do emerytury zostało Ci " + (67 - wiek) + "lat", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko Emerytura");
+                }
+                else if (wiek < 0)
+                {
+                    Interaction.MsgBox("Błędne dane!", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+                }
+                else
+                {
+                    Interaction.MsgBox("Jesteś emerytem", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+                }
+            }
+            catch (FormatException)
+            {
+                Interaction.MsgBox("Koniec", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Koniec");
             }
         }
     }
