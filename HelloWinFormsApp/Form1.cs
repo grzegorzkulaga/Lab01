5e08a125d5ab2a1a169e247b688e2615db6e05d6
grzegorzkulaga <poczta@grzegorzkulaga.pl>
2019-03-28 19:04:52 +0100

    HelloWinFormsApp interakcja

--- HelloWinFormsApp/Form1.cs
+++ HelloWinFormsApp/Form1.cs
@@ -29,7 +29,18 @@ namespace HelloWinFormsApp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            labelKomunikat.Text = "Witaj, " + textBoxImie.Text + " " + textBoxNazwisko.Text;
 
+            int wiek = Convert.ToInt32(textBoxWiek.Text);
+
+            if( wiek < 67 )
+            {
+                labelKomunikat2.Text = string.Format(" do emerytury zostało Ci {0} lat", 67 - wiek);
+            }
+            else
+            {
+                labelKomunikat2.Text = " jesteś emerytem ";
+            }
         }
 
         private void Witaj_Load(object sender, EventArgs e)
--- HelloWinFormsApp/Program.cs
+++ HelloWinFormsApp/Program.cs
@@ -16,7 +16,7 @@ namespace HelloWinFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Witaj());
         }
     }
 }
